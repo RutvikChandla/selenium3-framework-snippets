9b704040a6ec5c348fd8f8e41b9e6f69f6547c58
Rutvik Chandla <97675949+RutvikChandla@users.noreply.github.com>
2022-02-22 12:43:10 +0530

    Create cucumber-ruby-steps.rb

---
+++ cucumber-ruby-steps.rb
@@ -0,0 +1,22 @@
+Given /^I am on (.+)$/ do |url|
+  visit "https://www.bstackdemo.com/"
+end
+ 
+When "Click on add to cart button" do 
+  expect(title).to eq("StackDemo")
+  # Get product text
+  @productOnPageText = find(:xpath, '//*[@id="1"]/p').text
+
+  # Click on add to cart button
+  find(:xpath, '//*[@id="1"]/div[4]').click
+end
+ 
+Then "I should see the cart open and product added to it" do
+  # If cart is open or not
+  find(:xpath, '//*[@class="float-cart__content"]').visible?
+
+  # Get product text
+  @productOnCartText = find(:xpath, '//*[@id="__next"]/div/div/div[2]/div[2]/div[2]/div/div[3]/p[1]').text
+
+  expect(@productOnCartText).to eq(@productOnPageText)
+end
